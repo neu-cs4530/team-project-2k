dec6836124d6a63ffeb51a64361bc1e0ddd65475
John <18courj@gmail.com>
2022-03-28 17:07:43 -0400

    Adds player fields and getter/setter functions to backend

---
+++ services/townService/src/types/Player.test.ts
@@ -0,0 +1,23 @@
+import Player from '../types/Player';
+
+
+describe('Player', () => {
+  it('should initialize with appropriate fields', () => {
+    const testPlayer = new Player('test');
+    expect(testPlayer.location).toBeTruthy();
+    expect(testPlayer.userName).toBe('test');
+    expect(testPlayer.activeConversationArea).toBe(undefined);
+    expect(testPlayer.currentSong).toBe(undefined);
+    expect(testPlayer.selectedPlaylist).toBe(undefined);
+    expect(testPlayer.spotifyUsername).toBe(undefined);
+  })
+  it('should set fields with appropriate values via setter functions', () => {
+    const testPlayer = new Player('test');
+    testPlayer.currentSong = 'This World is On Fire By Alicia KEYS';
+    expect(testPlayer.currentSong).toBe('This World is On Fire By Alicia KEYS');
+    testPlayer.selectedPlaylist = 'test - Pavan';
+    expect(testPlayer.selectedPlaylist).toBe('test - Pavan');
+    testPlayer.spotifyUsername = 'spotted flamingo 2'
+    expect(testPlayer.spotifyUsername).toBe('spotted flamingo 2');
+  })
+})
