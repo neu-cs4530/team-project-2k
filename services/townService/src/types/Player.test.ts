e10ee55c536b0254212475bce3c417b49f5d4fe0
Pavan Hirpara <hirpara@Pavans-MacBook-Pro.local>
2022-04-22 18:26:10 -0400

    Add testing of API calls

--- services/townService/src/types/Player.test.ts
+++ services/townService/src/types/Player.test.ts
@@ -1,6 +1,5 @@
 import Player from './Player';
 
-
 describe('Player', () => {
   it('should initialize with appropriate fields', () => {
     const testPlayer = new Player('test');
@@ -19,5 +18,45 @@ describe('Player', () => {
     expect(testPlayer.selectedPlaylist).toBe('test - Pavan');
     testPlayer.spotifyUsername = 'spotted flamingo 2';
     expect(testPlayer.spotifyUsername).toBe('spotted flamingo 2');
+  });
+  it('should set Spotify related fields when given a valid Spotify token', async () => {
+    const testPlayer = new Player('foo', 'valid-token');
+    jest.spyOn(testPlayer, 'loadData').mockImplementation(async () => {
+      if (testPlayer.spotifyApi) {
+        testPlayer.currentSong = 'Party in the USA';
+        testPlayer.selectedPlaylist = 'Throwbacks';
+        testPlayer.spotifyUsername = 'MileyCyrusLover';
+      }
+    });
+
+    expect(testPlayer.currentSong).toBe(undefined);
+    expect(testPlayer.selectedPlaylist).toBe(undefined);
+    expect(testPlayer.spotifyUsername).toBe(undefined);
+
+    await testPlayer.loadData();
+
+    expect(testPlayer.currentSong).toBe('Party in the USA');
+    expect(testPlayer.selectedPlaylist).toBe('Throwbacks');
+    expect(testPlayer.spotifyUsername).toBe('MileyCyrusLover');
+  });
+  it('should not set Spotify related fields missing a valid Spotify token', async () => {
+    const testPlayer = new Player('foo', null);
+    jest.spyOn(testPlayer, 'loadData').mockImplementation(async () => {
+      if (testPlayer.spotifyApi) {
+        testPlayer.currentSong = 'Party in the USA';
+        testPlayer.selectedPlaylist = 'Throwbacks';
+        testPlayer.spotifyUsername = 'MileyCyrusLover';
+      }
+    });
+
+    expect(testPlayer.currentSong).toBe(undefined);
+    expect(testPlayer.selectedPlaylist).toBe(undefined);
+    expect(testPlayer.spotifyUsername).toBe(undefined);
+
+    await testPlayer.loadData();
+
+    expect(testPlayer.currentSong).toBe(undefined);
+    expect(testPlayer.selectedPlaylist).toBe(undefined);
+    expect(testPlayer.spotifyUsername).toBe(undefined);
   });
 });
